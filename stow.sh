0c722e56337bbaaae376218dc3d97e9c012af07e
Bjarte Klyve Larsen <bjarteklarsen@gmail.com>
2022-04-10 00:14:07 +0200

    added submodule stow to stow.sh

--- stow.sh
+++ stow.sh
@@ -9,9 +9,26 @@ folders=(
   "git"
   "gh"
 )
+submodules=(
+  "dotfiles-rakentaa"
+  "dotfiles-maritimeoptima"
+)
 
 for folder in "${folders[@]}"
 do
   echo "Stowing $folder"
+  stow -D $folder
   stow $folder
+done
+
+for submodule in "${submodules[@]}"
+do
+  echo "----------------------------"
+  echo "Stowing submodule $submodule"
+  if [ -f "$submodule/stow.sh" ]; then
+    echo "Running stow.sh in $submodule"
+    $submodule/stow.sh
+  else
+    echo -e "\tNo stow.sh found in $submodule"
+  fi
 done
