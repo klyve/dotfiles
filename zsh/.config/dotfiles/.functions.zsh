c1492ebbb0adc57ba0d8e59dcf6b9feb1b1a31d0
Bjarte Klyve Larsen <bjarteklarsen@gmail.com>
2022-04-27 12:36:38 +0200

    removed unused functions

--- zsh/.config/dotfiles/.functions.zsh
+++ zsh/.config/dotfiles/.functions.zsh
@@ -309,23 +309,3 @@ function vi_mode_prompt_info() {
 if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
   RPS1='$(vi_mode_prompt_info)'
 fi
-
-function jql() {
-  curl "https://${JIRA_TEAM}.atlassian.net/rest/api/2/search?jql=$1&maxResults=100&startAt=$(($2 * 100))" \
-    -s -u "${JIRA_USER}:${JIRA_TOKEN}" \
-    |  jq -r '.issues[] | "\(.key): \(.fields.summary)"'
-}
-
-# type = Subtask and parent = "MR-466" and status != Done
-# Child issues
-function fjql() {
-  ticket=$({for ((req=0;req<5;req++)); do
-    jql $1 $req &
-  done} | fzf --no-hscroll +m | cut -d ':' -f 1)
-  [ ${#ticket} -eq 0 ] return
-
-  open "https://${JIRA_TEAM}.atlassian.net/browse/${ticket}"
-}
-
-alias fj="fjql 'order%20by%20updated%20DESC'"
-alias fjm="fjql 'project%20%3D%20%22${JIRA_PROJECT}%22%20AND%20assignee%20%3D%20currentUser%28%29%20AND%20sprint%20in%20openSprints%28%29%20AND%20resolution%20%3D%20Unresolved%20order%20by%20updated%20DESC'"
